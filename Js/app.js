79f70de629a0207f71ab8d08af669c5980ecddb5
BatoolBtoush <batoolbtoush98@gmail.com>
2022-01-21 00:50:02 +0200

    added a javascript file

---
+++ Js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+var name = prompt('Please enter your name');
+console.log('The user name is: ' + name);
+
+
+var gender = prompt('Please enter your gender', "Male/Female");
+console.log('the user gender is: ' + gender);
+
+
+var age = prompt('Please enter your age');
+if (age <= 0) {
+    alert('Oops! Your age is less or equal to zero which is impossible. Fix it please.');
+}
+
+
+var conf = confirm('Do you want to skip the welcoming message?');
+if (conf == true) {
+    console.log('confirmation is '+ conf);
+} else if (conf == false  &&  gender == 'Male' || gender == 'male') {
+    alert('Welcome to the page Mr '+ name);
+} else if (conf == false  &&  gender == 'Female' || gender == 'female' ) {
+    alert('Welcome to the page Ms '+ name);
+} else {
+    alert('Welcome to the page '+name);
+}
+
+
+
+
+
+
+
